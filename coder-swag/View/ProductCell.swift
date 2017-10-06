65191c6e7a060e71a4c4ee355b49ef6e1a2b289d
Luis Calvillo <luiscalvillo@Luiss-MacBook-Pro.local>
2017-10-06 09:05:13 -0700

    Added collection view

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Luis Calvillo on 10/6/17.
+//  Copyright © 2017 Luis Calvillo. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
